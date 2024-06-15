a592fa2ececad1810814618c8a5ed5629af6daca
saisudhir14 <sudhirdontha@outlook.com>
2024-06-15 01:37:25 -0400

    -- add interval

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 async function generateRandomMeme() {
+    let img = document.querySelector(".meme img")
     let res = await fetch("https://meme-api.com/gimme")
     console.log(res)
     let json = await res.json()
     console.log(json)
+    let url = await json.url
+    console.log(url)
+    img.src = url;
 
 }
 generateRandomMeme();
+let time = setInterval(generateRandomMeme,7000);
+
+img.addEventListener("mouseover",function(){
+    clearInterval(time);
+})
